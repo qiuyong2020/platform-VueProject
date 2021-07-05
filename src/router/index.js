2ce566d8d96bb80db1244e40b08ce28550eea51e
qiuyong2020 <3037271525@qq.com>
2021-07-05 20:54:57 +0800

    配置文件别名

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'  //导入路由
 
-import Content from '../components/Content'
-import Main from '../components/Main'
+
 
 
 // 安装路由
@@ -10,20 +9,5 @@ Vue.use(VueRouter);
 
 // 配置导出路由
 export default new VueRouter({
-    routes: [
-        {
-             //路由路径
-             path: '/content',
-             name: 'content',
-             //跳转的组件
-             component: Content
-        },
-        {
-            //路由路径
-            path: '/main',
-            name: 'main',
-            //跳转的组件
-            component: Main
-       }
-    ]
+    routes: []
 })
